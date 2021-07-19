1805d1cfadd1ab33abe2ae4019fc9329c5faa7e7
sejigner <sejigner@gmail.com>
2021-07-19 20:48:39 +0900

    Add TextWatcher to InitialSetup

--- app/src/main/java/com/sejigner/closest/InitialSetupActivity.kt
+++ app/src/main/java/com/sejigner/closest/InitialSetupActivity.kt
@@ -1,7 +1,10 @@
 package com.sejigner.closest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Toast
 import com.google.android.gms.tasks.OnSuccessListener
@@ -9,6 +12,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_initial_setup.*
+import java.util.*
 
 class InitialSetupActivity : AppCompatActivity() {
     private var fireBaseAuth: FirebaseAuth? = null
@@ -16,19 +20,22 @@ class InitialSetupActivity : AppCompatActivity() {
     private var fbFireStore: FirebaseFirestore? = null
     private var uid: String? = null
     private var lastTimePressed = 0L
+    private var gender: String? = null
+    private var birthYear: String? = null
+    private var nickname : String? = null
+    val c = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_initial_setup)
-
-        var gender: String? = null
-        var birthYear: String? = null
-        var nickname : String? = null
-
+        
         fireBaseAuth = FirebaseAuth.getInstance()
         fireBaseUser = fireBaseAuth!!.currentUser
         fbFireStore = FirebaseFirestore.getInstance()
         uid = fireBaseAuth!!.currentUser?.uid
+        // 닉네임 입력 감지 리스너
+        et_nickname.addTextChangedListener(textWatcherNickname)
+
 
         rb_female.setOnClickListener {
             gender = "female"
@@ -45,12 +52,31 @@ class InitialSetupActivity : AppCompatActivity() {
                 // 개인정보 확인 다이얼로그 구현
 
                 setTrueInitialSetup()
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
             }
         }
 
 
     }
 
+    private val textWatcherNickname = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            TODO("Not yet implemented")
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+            nickname = s.toString()
+        }
+    }
+
+    // 파이어스토어 User 정보의 isInitialSetup 값을 True로 설정하여 최초 1회 설정만 가능하게 함
     private fun setTrueInitialSetup() {
 
         fbFireStore = FirebaseFirestore.getInstance()
@@ -72,5 +98,4 @@ class InitialSetupActivity : AppCompatActivity() {
             Toast.makeText(this, "앱을 종료하시려면 뒤로가기 버튼을 두번 터치해주세요.", Toast.LENGTH_SHORT).show()
         lastTimePressed = System.currentTimeMillis()
     }
-}
 }
