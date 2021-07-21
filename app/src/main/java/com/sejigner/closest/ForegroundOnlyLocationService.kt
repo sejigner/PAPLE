e1e03af2fd56fddb2627dc57415de18ca268489f
sejigner <sejigner@gmail.com>
2021-07-21 12:23:36 +0900

    import codelabs sample codes for FusedLocationProviderClient.

--- app/src/main/java/com/sejigner/closest/ForegroundOnlyLocationService.kt
+++ app/src/main/java/com/sejigner/closest/ForegroundOnlyLocationService.kt
@@ -1,26 +1,29 @@
 package com.sejigner.closest
 
-import android.app.NotificationManager
-import android.app.Service
+import android.app.*
 import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.location.Location
+import android.os.Binder
+import android.os.Build
+import android.os.IBinder
+import android.os.Looper
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.google.android.gms.location.*
-import com.sejigner.closest.MainActivity.Companion.TAG
 import java.util.concurrent.TimeUnit
-
 class ForegroundOnlyLocationService : Service() {
-    //https://codelabs.developers.google.com/codelabs/while-in-use-location/#3
+    // https://codelabs.developers.google.com/codelabs/while-in-use-location/#3
     /*
      * Checks whether the bound activity has really gone away (foreground service with notification
      * created) or simply orientation change (no-op).
      */
-    private var configurationChang = false
+    private var configurationChange = false
     private var serviceRunningInForeground = false
     private val localBinder = LocalBinder()
-    private lateinit var notificationManager = NotificationManager
+    private lateinit var notificationManager : NotificationManager
 
     // FusedLocationProviderClient - Main class for receiving location updates.
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
@@ -38,7 +41,7 @@ class ForegroundOnlyLocationService : Service() {
     private var currentLocation: Location? = null
 
     override fun onCreate() {
-        Log.d(TAG,"onCreate()")
+        Log.d("locationService","onCreate()")
 
         notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
@@ -94,8 +97,8 @@ class ForegroundOnlyLocationService : Service() {
         }
     }
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Log.d(TAG, "onStartCommand()")
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        Log.d("locationService", "onStartCommand()")
 
         val cancelLocationTrackingFromNotification =
             intent.getBooleanExtra(EXTRA_CANCEL_LOCATION_TRACKING_FROM_NOTIFICATION, false)
@@ -104,10 +107,191 @@ class ForegroundOnlyLocationService : Service() {
             unsubscribeToLocationUpdates()
             stopSelf()
         }
+        // Tells the system not to recreate the service after it's been killed.
+        return START_NOT_STICKY
     }
 
-    inner class LocalBinder {
+    override fun onBind(intent: Intent): IBinder {
+        Log.d("locationService", "onBind()")
 
+        // MainActivity (client) comes into foreground and binds to service, so the service can
+        // become a background services.
+        stopForeground(true)
+        serviceRunningInForeground = false
+        configurationChange = false
+        return localBinder
+    }
+
+    override fun onRebind(intent: Intent) {
+        Log.d("locationService", "onRebind()")
+
+        // MainActivity (client) returns to the foreground and rebinds to service, so the service
+        // can become a background services.
+        stopForeground(true)
+        serviceRunningInForeground = false
+        configurationChange = false
+        super.onRebind(intent)
+    }
+
+    override fun onUnbind(intent: Intent): Boolean {
+        Log.d("locationService", "onUnbind()")
+
+        // MainActivity (client) leaves foreground, so service needs to become a foreground service
+        // to maintain the 'while-in-use' label.
+        // NOTE: If this method is called due to a configuration change in MainActivity,
+        // we do nothing.
+        if (!configurationChange && SharedPreferenceUtil.getLocationTrackingPref(this)) {
+            Log.d("locationService", "Start foreground service")
+            val notification = generateNotification(currentLocation)
+            startForeground(NOTIFICATION_ID, notification)
+            serviceRunningInForeground = true
+        }
+        // Ensures onRebind() is called if MainActivity (client) rebinds.
+        return true
+    }
+
+    override fun onDestroy() {
+        Log.d("locationService", "onDestroy()")
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        configurationChange = true
+    }
+
+    fun subscribeToLocationUpdates() {
+        Log.d("locationService", "subscribeToLocationUpdates()")
+
+        SharedPreferenceUtil.saveLocationTrackingPref(this, true)
+
+        // Binding to this service doesn't actually trigger onStartCommand(). That is needed to
+        // ensure this Service can be promoted to a foreground service, i.e., the service needs to
+        // be officially started (which we do here).
+        startService(Intent(applicationContext, ForegroundOnlyLocationService::class.java))
+
+        try {
+            // TODO: Step 1.5, Subscribe to location changes.
+            fusedLocationProviderClient.requestLocationUpdates(
+                locationRequest, locationCallback, Looper.getMainLooper())
+        } catch (unlikely: SecurityException) {
+            SharedPreferenceUtil.saveLocationTrackingPref(this, false)
+            Log.e("locationService", "Lost location permissions. Couldn't remove updates. $unlikely")
+        }
+    }
+
+    fun unsubscribeToLocationUpdates() {
+        Log.d("locationService", "unsubscribeToLocationUpdates()")
+
+        try {
+            // TODO: Step 1.6, Unsubscribe to location changes.
+            val removeTask = fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+            removeTask.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d("locationService", "Location Callback removed.")
+                    stopSelf()
+                } else {
+                    Log.d("locationService", "Failed to remove Location Callback.")
+                }
+            }
+            SharedPreferenceUtil.saveLocationTrackingPref(this, false)
+        } catch (unlikely: SecurityException) {
+            SharedPreferenceUtil.saveLocationTrackingPref(this, true)
+            Log.e("locationService", "Lost location permissions. Couldn't remove updates. $unlikely")
+        }
+    }
+
+    /*
+     * Generates a BIG_TEXT_STYLE Notification that represent latest location.
+     */
+    private fun generateNotification(location: Location?): Notification {
+        Log.d("locationService", "generateNotification()")
+
+        // Main steps for building a BIG_TEXT_STYLE notification:
+        //      0. Get data
+        //      1. Create Notification Channel for O+
+        //      2. Build the BIG_TEXT_STYLE
+        //      3. Set up Intent / Pending Intent for notification
+        //      4. Build and issue the notification
+
+        // 0. Get data
+        val mainNotificationText = location?.toText() ?: getString(R.string.no_location_text)
+        val titleText = getString(R.string.app_name)
+
+        // 1. Create Notification Channel for O+ and beyond devices (26+).
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val notificationChannel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID, titleText, NotificationManager.IMPORTANCE_DEFAULT)
+
+            // Adds NotificationChannel to system. Attempting to create an
+            // existing notification channel with its original values performs
+            // no operation, so it's safe to perform the below sequence.
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
+        // 2. Build the BIG_TEXT_STYLE.
+        val bigTextStyle = NotificationCompat.BigTextStyle()
+            .bigText(mainNotificationText)
+            .setBigContentTitle(titleText)
+
+        // 3. Set up main Intent/Pending Intents for notification.
+        val launchActivityIntent = Intent(this, MainActivity::class.java)
+
+        val cancelIntent = Intent(this, ForegroundOnlyLocationService::class.java)
+        cancelIntent.putExtra(EXTRA_CANCEL_LOCATION_TRACKING_FROM_NOTIFICATION, true)
+
+        val servicePendingIntent = PendingIntent.getService(
+            this, 0, cancelIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val activityPendingIntent = PendingIntent.getActivity(
+            this, 0, launchActivityIntent, 0)
+
+        // 4. Build and issue the notification.
+        // Notification Channel Id is ignored for Android pre O (26).
+        val notificationCompatBuilder =
+            NotificationCompat.Builder(applicationContext, NOTIFICATION_CHANNEL_ID)
+
+        return notificationCompatBuilder
+            .setStyle(bigTextStyle)
+            .setContentTitle(titleText)
+            .setContentText(mainNotificationText)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setOngoing(true)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .addAction(
+                R.drawable.ic_launcher_foreground, getString(R.string.launch_activity),
+                activityPendingIntent
+            )
+            .addAction(
+                R.drawable.ic_cancel,
+                getString(R.string.stop_location_updates_button_text),
+                servicePendingIntent
+            )
+            .build()
+    }
+
+    inner class LocalBinder : Binder() {
+        internal val service : ForegroundOnlyLocationService
+        get() = this@ForegroundOnlyLocationService
+    }
+
+    companion object {
+        private const val locationService = "yLocationService"
+
+        private const val PACKAGE_NAME = "com.example.android.whileinuselocation"
+
+        internal const val ACTION_FOREGROUND_ONLY_LOCATION_BROADCAST =
+            "$PACKAGE_NAME.action.FOREGROUND_ONLY_LOCATION_BROADCAST"
+
+        internal const val EXTRA_LOCATION = "$PACKAGE_NAME.extra.LOCATION"
+
+        private const val EXTRA_CANCEL_LOCATION_TRACKING_FROM_NOTIFICATION =
+            "$PACKAGE_NAME.extra.CANCEL_LOCATION_TRACKING_FROM_NOTIFICATION"
+
+        private const val NOTIFICATION_ID = 12345678
+
+        private const val NOTIFICATION_CHANNEL_ID = "while_in_use_channel_01"
     }
 }
 
--- app/src/main/java/com/sejigner/closest/fragment/FragmentHome.kt
+++ app/src/main/java/com/sejigner/closest/fragment/FragmentHome.kt
@@ -1,41 +1,47 @@
 package com.sejigner.closest.fragment
 
 import android.Manifest
-import android.content.Context
-import android.content.Intent
+import android.content.*
 import android.content.pm.PackageManager
 import android.location.*
+import android.net.Uri
 import android.os.Bundle
+import android.os.IBinder
 import android.provider.Settings
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.firebase.geofire.GeoFire
 import com.firebase.geofire.GeoLocation
 import com.google.android.gms.common.api.GoogleApi
 import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.FirebaseFirestore
-import com.sejigner.closest.MainActivity
+import com.sejigner.closest.*
 import com.sejigner.closest.MainActivity.Companion.TAG
-import com.sejigner.closest.R
-import com.sejigner.closest.Users
+import com.sejigner.closest.SharedPreferenceUtil
 import kotlinx.android.synthetic.main.fragment_home.*
 import java.io.IOException
 import java.util.*
 
 
-class FragmentHome : Fragment() {
+class FragmentHome : Fragment(), SharedPreferences.OnSharedPreferenceChangeListener {
+
+
 
     private var fireBaseAuth : FirebaseAuth? = null
     private var fireBaseUser : FirebaseUser? = null
@@ -50,6 +56,35 @@ class FragmentHome : Fragment() {
     private var locationNetwork : Location? = null
     private var currentCoordinates : Location? = null
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private var foregroundOnlyLocationServiceBound = false
+
+    // Provides location updates for while-in-use feature.
+    private var foregroundOnlyLocationService: ForegroundOnlyLocationService? = null
+
+    // Listens for location broadcasts from ForegroundOnlyLocationService.
+    private lateinit var foregroundOnlyBroadcastReceiver: ForegroundOnlyBroadcastReceiver
+
+    private lateinit var sharedPreferences: SharedPreferences
+
+    private lateinit var foregroundOnlyLocationButton: Button
+
+    private lateinit var outputTextView: TextView
+
+    // Monitors connection to the while-in-use service.
+    private val foregroundOnlyServiceConnection = object : ServiceConnection {
+
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            val binder = service as ForegroundOnlyLocationService.LocalBinder
+            foregroundOnlyLocationService = binder.service
+            foregroundOnlyLocationServiceBound = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName) {
+            foregroundOnlyLocationService = null
+            foregroundOnlyLocationServiceBound = false
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?)
     : View? {
@@ -104,6 +139,191 @@ class FragmentHome : Fragment() {
             tv_address.text = getLocation()
         }
 
+        foregroundOnlyBroadcastReceiver = ForegroundOnlyBroadcastReceiver()
+
+        sharedPreferences =
+            requireActivity().getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
+
+        foregroundOnlyLocationButton = findViewById(R.id.foreground_only_location_button)
+        outputTextView = findViewById(R.id.output_text_view)
+
+        foregroundOnlyLocationButton.setOnClickListener {
+            val enabled = sharedPreferences.getBoolean(
+                SharedPreferenceUtil.KEY_FOREGROUND_ENABLED, false)
+
+            if (enabled) {
+                foregroundOnlyLocationService?.unsubscribeToLocationUpdates()
+            } else {
+                // TODO: Step 1.0, Review Permissions: Checks and requests if needed.
+                if (foregroundPermissionApproved()) {
+                    foregroundOnlyLocationService?.subscribeToLocationUpdates()
+                        ?: Log.d(TAG, "Service Not Bound")
+                } else {
+                    requestForegroundPermissions()
+                }
+            }
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        updateButtonState(
+            sharedPreferences.getBoolean(SharedPreferenceUtil.KEY_FOREGROUND_ENABLED, false)
+        )
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+
+        val serviceIntent = Intent(requireActivity(), ForegroundOnlyLocationService::class.java)
+        bindService(serviceIntent, foregroundOnlyServiceConnection, Context.BIND_AUTO_CREATE)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        LocalBroadcastManager.getInstance(this).registerReceiver(
+            foregroundOnlyBroadcastReceiver,
+            IntentFilter(
+                ForegroundOnlyLocationService.ACTION_FOREGROUND_ONLY_LOCATION_BROADCAST)
+        )
+    }
+
+    override fun onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(
+            foregroundOnlyBroadcastReceiver
+        )
+        super.onPause()
+    }
+
+    override fun onStop() {
+        if (foregroundOnlyLocationServiceBound) {
+            unbindService(foregroundOnlyServiceConnection)
+            foregroundOnlyLocationServiceBound = false
+        }
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+
+        super.onStop()
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
+        // Updates button states if new while in use location is added to SharedPreferences.
+        if (key == SharedPreferenceUtil.KEY_FOREGROUND_ENABLED) {
+            updateButtonState(sharedPreferences.getBoolean(
+                SharedPreferenceUtil.KEY_FOREGROUND_ENABLED, false)
+            )
+        }
+    }
+
+    // TODO: Step 1.0, Review Permissions: Method checks if permissions approved.
+    private fun foregroundPermissionApproved(): Boolean {
+        return PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    }
+
+    // TODO: Step 1.0, Review Permissions: Method requests permissions.
+    private fun requestForegroundPermissions() {
+        val provideRationale = foregroundPermissionApproved()
+
+        // If the user denied a previous request, but didn't check "Don't ask again", provide
+        // additional rationale.
+        if (provideRationale) {
+            Snackbar.make(
+                findViewById(R.id.activity_main),
+                R.string.permission_rationale,
+                Snackbar.LENGTH_LONG
+            )
+                .setAction(R.string.ok) {
+                    // Request permission
+                    ActivityCompat.requestPermissions(
+                        this@MainActivity,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE
+                    )
+                }
+                .show()
+        } else {
+            Log.d(TAG, "Request foreground only permission")
+            ActivityCompat.requestPermissions(
+                this@MainActivity,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE
+            )
+        }
+    }
+
+    // TODO: Step 1.0, Review Permissions: Handles permission result.
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        Log.d(TAG, "onRequestPermissionResult")
+
+        when (requestCode) {
+            REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE -> when {
+                grantResults.isEmpty() ->
+                    // If user interaction was interrupted, the permission request
+                    // is cancelled and you receive empty arrays.
+                    Log.d(TAG, "User interaction was cancelled.")
+                grantResults[0] == PackageManager.PERMISSION_GRANTED ->
+                    // Permission was granted.
+                    foregroundOnlyLocationService?.subscribeToLocationUpdates()
+                else -> {
+                    // Permission denied.
+                    updateButtonState(false)
+
+                    Snackbar.make(
+                        findViewById(R.id.activity_main),
+                        R.string.permission_denied_explanation,
+                        Snackbar.LENGTH_LONG
+                    )
+                        .setAction(R.string.settings) {
+                            // Build intent that displays the App settings screen.
+                            val intent = Intent()
+                            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                            val uri = Uri.fromParts(
+                                "package",
+                                BuildConfig.APPLICATION_ID,
+                                null
+                            )
+                            intent.data = uri
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                            startActivity(intent)
+                        }
+                        .show()
+                }
+            }
+        }
+    }
+
+    private fun updateButtonState(trackingLocation: Boolean) {
+        if (trackingLocation) {
+            foregroundOnlyLocationButton.text = getString(R.string.stop_location_updates_button_text)
+        } else {
+            foregroundOnlyLocationButton.text = getString(R.string.start_location_updates_button_text)
+        }
+    }
+
+    private fun logResultsToScreen(output: String) {
+        val outputWithPreviousLogs = "$output\n${outputTextView.text}"
+        outputTextView.text = outputWithPreviousLogs
+    }
+
+    /**
+     * Receiver for location broadcasts from [ForegroundOnlyLocationService].
+     */
+    private inner class ForegroundOnlyBroadcastReceiver : BroadcastReceiver() {
+
+        override fun onReceive(context: Context, intent: Intent) {
+            val location = intent.getParcelableExtra<Location>(
+                ForegroundOnlyLocationService.EXTRA_LOCATION
+            )
+
+            if (location != null) {
+                logResultsToScreen("Foreground location: ${location.toText()}")
+            }
+        }
     }
 
     private fun getCoordinates() {
@@ -220,6 +440,10 @@ class FragmentHome : Fragment() {
         }
         return currentLocation
     }
+
+
+
+
 }
 
 
