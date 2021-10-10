f53b4642803846a4183db0c920983e16002593c2
sejin <sejigner@gmail.com>
2021-10-10 21:30:02 +0900

    Set up the service for FCM.

--- app/src/main/java/com/sejigner/closest/FirebaseMessagingService.kt
+++ app/src/main/java/com/sejigner/closest/FirebaseMessagingService.kt
@@ -1,42 +1,77 @@
 package com.sejigner.closest
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.media.Ringtone
+import android.media.RingtoneManager
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import kotlin.random.Random
 
 class FirebaseMessagingService : FirebaseMessagingService() {
-    override fun onNewToken(token: String) {
-        val refreshedToken = FirebaseMessaging.getInstance().token
-        Log.d(TAG, "Refreshed token: $token")
 
-        val pref = this.getSharedPreferences("token", Context.MODE_PRIVATE)
-        val editor = pref.edit()
-        editor.putString("token", token).apply()
-        editor.commit()
-
-        Log.i(TAG, "saved token successfully")
-    }
+    private val ADMIN_CHANNEL_ID = "admin_channel"
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
         Log.d(TAG, "From: " + remoteMessage.from)
 
-        if(remoteMessage.data.isNotEmpty()) {
-            Log.i("바디", remoteMessage.data["body"].toString())
-            Log.i("타이틀",remoteMessage.data["title"].toString())
-            sendNotification(remoteMessage)
+        val intent = Intent(this, MainActivity::class.java)
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationID = Random.nextInt(3000)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            setupChannels(notificationManager)
         }
 
-        else {
-            Log.i("수신에러","data가 비어있습니다. 메시지를 수신하지 못했습니다.")
-            Log.i("data값", remoteMessage.data.toString())
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent, PendingIntent.FLAG_ONE_SHOT
+        )
+
+        val largeIcon = BitmapFactory.decodeResource(
+            resources,
+            R.drawable.ic_paper_plane
+        )
+
+        val notificationSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, ADMIN_CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_paper_plane)
+            .setLargeIcon(largeIcon)
+            .setContentTitle(remoteMessage.data.get("title"))
+            .setContentText(remoteMessage.data.get("message"))
+            .setAutoCancel(true)
+            .setSound(notificationSoundUri)
+            .setContentIntent(pendingIntent)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            notificationBuilder.color = resources.getColor(R.color.bg_initialSetting)
         }
+        notificationManager.notify(notificationID, notificationBuilder.build())
+
     }
 
-    private fun sendNotification(remoteMessage: RemoteMessage) {
-        
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private fun setupChannels(notificationManager: NotificationManager?) {
+        val adminChannelName = "New notification"
+        val adminChannelDescription = "Device to device notification"
+
+        val adminChannel : NotificationChannel
+        adminChannel = NotificationChannel(ADMIN_CHANNEL_ID, adminChannelName, NotificationManager.IMPORTANCE_HIGH)
+        adminChannel.description = adminChannelDescription
+        adminChannel.enableLights(true)
+        adminChannel.lightColor = Color.BLUE
+        adminChannel.enableVibration(true)
+        notificationManager?.createNotificationChannel(adminChannel)
     }
 
 
