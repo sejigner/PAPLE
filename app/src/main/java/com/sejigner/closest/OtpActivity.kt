59cb9809753e3a872fceeae4366cbbc6100e8d85
sejin <sejigner@gmail.com>
2021-11-18 10:37:13 +0900

    Improve UI on OtpActivity

--- app/src/main/java/com/sejigner/closest/OtpActivity.kt
+++ app/src/main/java/com/sejigner/closest/OtpActivity.kt
@@ -1,6 +1,7 @@
 package com.sejigner.closest
 
 import android.content.Intent
+import android.graphics.Paint
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.TextView
@@ -11,12 +12,14 @@ import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import com.google.firebase.auth.PhoneAuthCredential
 import com.google.firebase.auth.PhoneAuthProvider
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_otp.*
 
 class OtpActivity : AppCompatActivity() {
 
     // get reference of the firebase auth
     lateinit var auth: FirebaseAuth
     lateinit var fbFirestore : FirebaseFirestore
+    var time_left : Int ? = 60
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,20 +28,27 @@ class OtpActivity : AppCompatActivity() {
         auth=FirebaseAuth.getInstance()
         fbFirestore=FirebaseFirestore.getInstance()
 
+        tv_request_resend.paintFlags = Paint.UNDERLINE_TEXT_FLAG
+
         // get storedVerificationId from the intent
         val storedVerificationId= intent.getStringExtra("storedVerificationId")
 
         // fill otp and call the on click on button
-        findViewById<TextView>(R.id.login).setOnClickListener {
+        cl_otp_check.setOnClickListener {
             val otp = findViewById<EditText>(R.id.et_otp).text.trim().toString()
             if(otp.isNotEmpty()){
                 val credential : PhoneAuthCredential = PhoneAuthProvider.getCredential(
                     storedVerificationId.toString(),otp)
                 signInWithPhoneAuthCredential(credential)
             } else {
-                Toast.makeText(this,"Enter OTP", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this,"인증 번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
             }
         }
+
+        tv_time_left.text = getString(R.string.otp_time, time_left)
+
+
+
     }
     // verifies if the code matches sent by firebase
     // if success start the main activity
