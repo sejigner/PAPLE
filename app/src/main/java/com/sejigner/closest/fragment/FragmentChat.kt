04687eea69d482bb7e91c5f49fc6774339b94338
sejigner <sejigner@gmail.com>
2021-12-22 22:24:55 +0900

    Make alert dialog fragment

--- app/src/main/java/com/sejigner/closest/fragment/FragmentChat.kt
+++ app/src/main/java/com/sejigner/closest/fragment/FragmentChat.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.view.size
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
