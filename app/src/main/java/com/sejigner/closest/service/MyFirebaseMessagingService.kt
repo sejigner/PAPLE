3c438b70d907a16b4e0f35f537a9b064c742eb9e
sejin <sejigner@gmail.com>
2021-12-27 10:58:30 +0900

    Implement data message notification

--- app/src/main/java/com/sejigner/closest/service/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/sejigner/closest/service/MyFirebaseMessagingService.kt
@@ -1,4 +1,5 @@
 package com.sejigner.closest.service
+
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -6,6 +7,7 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
 import android.graphics.Color
+import android.media.AudioAttributes
 import android.media.RingtoneManager
 import android.os.Build
 import android.util.Log
@@ -15,60 +17,86 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.sejigner.closest.App
 import com.sejigner.closest.MainActivity
 import com.sejigner.closest.MainActivity.Companion.UID
-import com.sejigner.closest.R
 import com.sejigner.closest.fragment.FragmentHome
 import kotlin.random.Random
+import androidx.core.app.NotificationManagerCompat
+import android.os.FileUtils
+import com.sejigner.closest.R
+
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     private val ADMIN_CHANNEL_ID = "admin_channel"
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-//        super.onMessageReceived(remoteMessage)
-//        Log.d(TAG, "From: " + remoteMessage.from)
-//
-//        val intent = Intent(this, MainActivity::class.java)
-//        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-//        val notificationID = Random.nextInt(3000)
-//
-//        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            setupChannels(notificationManager)
-//        }
-//
-//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-//        val pendingIntent = PendingIntent.getActivity(
-//            this, 0, intent, PendingIntent.FLAG_ONE_SHOT
-//        )
-//
-//        val largeIcon = BitmapFactory.decodeResource(
-//            resources,
-//            R.drawable.ic_paper_plane
-//        )
-//
-//        val notificationSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-//        val notificationBuilder = NotificationCompat.Builder(this, ADMIN_CHANNEL_ID)
-//            .setSmallIcon(R.drawable.ic_paper_plane)
-//            .setLargeIcon(largeIcon)
-//            .setContentTitle(remoteMessage.data["title"])
-//            .setContentText(remoteMessage.data["message"])
-//            .setAutoCancel(true)
-//            .setSound(notificationSoundUri)
-//            .setContentIntent(pendingIntent)
-//
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//            notificationBuilder.color = resources.getColor(R.color.bg_initialSetting)
-//        }
-//        notificationManager.notify(notificationID, notificationBuilder.build())
+        super.onMessageReceived(remoteMessage)
 
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: ${remoteMessage.from}")
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
+                // Handle message within 10 seconds
+                sendNotification(remoteMessage)
+
+        }
     }
 
+    private fun sendNotification(messageBody: RemoteMessage) {
+        val notificationManager = applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(applicationContext, 0, intent, 0)
+        val channelId = resources.getString(R.string.default_notification_channel_id)
+        val notification = NotificationCompat.Builder(applicationContext, channelId)
+            .setSmallIcon(R.mipmap.ic_launcher_round)
+            .setContentTitle(messageBody.data["title"])
+            .setContentText(messageBody.data["body"])
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setShowWhen(true)
+        notification.priority = NotificationCompat.PRIORITY_MAX
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notification.setChannelId(channelId)
+
+            val ringtoneManager = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+            val audioAttributeSet = AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_NOTIFICATION_RINGTONE)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build()
+
+            val channel = NotificationChannel(
+                channelId, "default", NotificationManager.IMPORTANCE_HIGH
+            )
+
+            channel.apply {
+                enableLights(true)
+                enableVibration(true)
+                setSound(ringtoneManager, audioAttributes)
+            }
+
+            notificationManager.createNotificationChannel(channel)
+        }
+        notificationManager.notify(0, notification.build())
+
+//        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+    }
+
+
     override fun onNewToken(token: String) {
-        val fbDatabase = FirebaseDatabase.getInstance().reference.child("Users").child(UID).child("registrationToken")
+        val fbDatabase = FirebaseDatabase.getInstance().reference.child("Users").child(UID)
+            .child("registrationToken")
         fbDatabase.removeValue()
         fbDatabase.child(token).setValue(true).addOnSuccessListener {
-            Log.d(FragmentHome.TAG,"updated fcmToken: $token")
+            Log.d(FragmentHome.TAG, "updated fcmToken: $token")
         }
     }
 
@@ -77,8 +105,12 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         val adminChannelName = "New notification"
         val adminChannelDescription = "Device to device notification"
 
-        val adminChannel : NotificationChannel
-        adminChannel = NotificationChannel(ADMIN_CHANNEL_ID, adminChannelName, NotificationManager.IMPORTANCE_HIGH)
+        val adminChannel: NotificationChannel
+        adminChannel = NotificationChannel(
+            ADMIN_CHANNEL_ID,
+            adminChannelName,
+            NotificationManager.IMPORTANCE_HIGH
+        )
         adminChannel.description = adminChannelDescription
         adminChannel.enableLights(true)
         adminChannel.lightColor = Color.BLUE
