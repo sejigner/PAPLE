ef30a6465715144ad4376dc8c3aa07427e30a527
sejin <sejigner@gmail.com>
2021-12-29 11:24:44 +0900

    Rename xml file fragment_dialog_second fragment_dialog_replied

--- app/src/main/java/com/sejigner/closest/fragment/RepliedDialogFragment.kt
+++ app/src/main/java/com/sejigner/closest/fragment/RepliedDialogFragment.kt
@@ -25,7 +25,7 @@ import com.sejigner.closest.room.*
 import com.sejigner.closest.ui.FragmentChatViewModel
 import com.sejigner.closest.ui.FragmentChatViewModelFactory
 import kotlinx.android.synthetic.main.fragment_dialog_first.*
-import kotlinx.android.synthetic.main.fragment_dialog_second.*
+import kotlinx.android.synthetic.main.fragment_dialog_replied.*
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.math.round
@@ -79,7 +79,7 @@ class FragmentDialogReplied : DialogFragment(), ReportPlaneDialogFragment.Replie
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dialog_second, container, false)
+        return inflater.inflate(R.layout.`fragment_dialog_replied.xml`, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
