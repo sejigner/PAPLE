71c5914b1e20da218dd785018ee729f356231f72
sejigner <sejigner@gmail.com>
2022-02-05 11:57:55 +0900

    Apply AD_UNIT_ID

--- app/src/main/java/com/gievenbeck/paple/App.kt
+++ app/src/main/java/com/gievenbeck/paple/App.kt
@@ -3,7 +3,7 @@ package com.gievenbeck.paple
 import android.app.Application
 
 // 테스트용 광고 단위 ID
-const val AD_UNIT_ID = "ca-app-pub-3940256099942544/1033173712"
+const val AD_UNIT_ID = "ca-app-pub-5118743253590971/6381726931"
 private const val LOG_TAG = "AppOpenAdManager"
 
 class App : Application() {
