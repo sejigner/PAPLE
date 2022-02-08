17bad9950bee33e7eb19d6c345a2add4d5692c16
sejigner <sejigner@gmail.com>
2022-02-08 16:59:16 +0900

    Fix data class ReportedChat

--- app/src/main/java/com/gievenbeck/paple/models/ReportedChat.kt
+++ app/src/main/java/com/gievenbeck/paple/models/ReportedChat.kt
@@ -3,6 +3,6 @@ package com.gievenbeck.paple.models
 import androidx.annotation.Keep
 
 @Keep
-class ReportedChat(val reporter: String, val partner : String, val date: String ) {
-    constructor() : this("","", "")
+class ReportedChat(val reporterNickname: String, val reporterUid: String, val partnerNickname : String, val partnerUid : String, val date: String ) {
+    constructor() : this("", "", "", "", "")
 }
